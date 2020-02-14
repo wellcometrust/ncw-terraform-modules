1bc21d9ead9150ead6093ba8b1cf7f286ab9ba43
wellingk <k.welling@wellcome.ac.uk>
2020-02-14 17:24:16 +0000

    fixed the other two in line with first

--- AWS/iam-instance-roles/wt-standard-instance-role-rds-s3/main.tf
+++ AWS/iam-instance-roles/wt-standard-instance-role-rds-s3/main.tf
@@ -20,52 +20,32 @@ resource "aws_iam_instance_profile" "wt-standard-instance-profile-rds-s3" {
   role = aws_iam_role.wt-standard-instance-role-rds-s3.name
 }
 
-# Policy and attachment for SSM
-resource "aws_iam_policy" "amazon-ec2-role-for-ssm" {
-  policy = "arn:aws:iam::aws:policy/service-role/AmazonEC2RoleforSSM"
-}
-
+# Policy attachment for SSM
 resource "aws_iam_role_policy_attachment" "amazon-ecs-role-for-ssm-attachnment" {
-  policy_arn = aws_iam_policy.amazon-ec2-role-for-ssm.arn
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2RoleforSSM"
   role       = aws_iam_role.wt-standard-instance-role-rds-s3.id
 }
 
-# Policy and attachment SSM Instance Core
-resource "aws_iam_policy" "amazon-ssm-managed-instance-core" {
-  policy = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
-}
-
+# Policy attachment SSM Instance Core
 resource "aws_iam_role_policy_attachment" "amazon-ssm-managed-instance-core-attachment" {
-  policy_arn = aws_iam_policy.amazon-ssm-managed-instance-core.arn
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
   role       = aws_iam_role.wt-standard-instance-role-rds-s3.id
 }
 
-# Policy and attachment for Cloud Watch Logging
-resource "aws_iam_policy" "cloudwatch-agent-server-policy" {
-  policy = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
-}
-
+# Policy attachment for Cloud Watch Logging
 resource "aws_iam_role_policy_attachment" "cloudwatch-agent-server-policy-attachment" {
-  policy_arn = aws_iam_policy.cloudwatch-agent-server-policy.arn
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
   role       = aws_iam_role.wt-standard-instance-role-rds-s3.id
 }
 
-# Policy and attachement to allow access to all S3
-resource "aws_iam_policy" "amazon-s3-full-access" {
-  policy = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
-}
-
+# Policy attachement to allow access to all S3
 resource "aws_iam_role_policy_attachment" "amazon-s3-full-access-attachment" {
-  policy_arn = aws_iam_policy.amazon-s3-full-access.arn
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
   role       = aws_iam_role.wt-standard-instance-role-rds-s3.id
 }
 
 # Policy and attachement to allow access to RDS
-resource "aws_iam_policy" "amazon-rds-full-access" {
-  policy = "arn:aws:iam::aws:policy/AmazonRDSFullAccess"
-}
-
 resource "aws_iam_role_policy_attachment" "amazon-rds-full-access-attachment" {
-  policy_arn = aws_iam_policy.amazon-rds-full-access.arn
+  policy_arn = "arn:aws:iam::aws:policy/AmazonRDSFullAccess"
   role       = aws_iam_role.wt-standard-instance-role-rds-s3.id
 }
