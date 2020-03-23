522425fee5da53b950cfc64370922727700771c4
Kate Welling <k.welling@wellcome.ac.uk>
2020-03-23 16:33:33 +0000

    Overlapping outputs fixed

--- AWS/iam-instance-roles/wt-standard-instance-role-s3/outputs.tf
+++ AWS/iam-instance-roles/wt-standard-instance-role-s3/outputs.tf
@@ -1,11 +1,11 @@
-output "wt-standard-instance-role-arn" {
+output "wt-standard-instance-role-s3-arn" {
   value = aws_iam_role.wt-standard-instance-role-s3.arn
 }
 
-output "wt-standard-instance-role-name" {
+output "wt-standard-instance-roles-s3-name" {
   value = aws_iam_role.wt-standard-instance-role-s3.name
 }
 
-output "wt-standard-instance-role-description" {
+output "wt-standard-instance-role-s3-description" {
   value = aws_iam_role.wt-standard-instance-role-s3.description
 }
