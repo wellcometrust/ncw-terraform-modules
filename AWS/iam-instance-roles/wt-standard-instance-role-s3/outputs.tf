78a9b65315d4a3c9597d98aeb114d6bbbecec782
Kate Welling <k.welling@wellcome.ac.uk>
2020-03-23 16:53:27 +0000

    one more little overlap

--- AWS/iam-instance-roles/wt-standard-instance-role-s3/outputs.tf
+++ AWS/iam-instance-roles/wt-standard-instance-role-s3/outputs.tf
@@ -2,7 +2,7 @@ output "wt-standard-instance-role-s3-arn" {
   value = aws_iam_role.wt-standard-instance-role-s3.arn
 }
 
-output "wt-standard-instance-roles-s3-name" {
+output "wt-standard-instance-role-s3-name" {
   value = aws_iam_role.wt-standard-instance-role-s3.name
 }
 
