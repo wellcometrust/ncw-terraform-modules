c2293fd347095860a9176befbc755a04490eab73
Kate Welling <k.welling@wellcome.ac.uk>
2020-04-08 17:50:58 +0100

    Fixed the error of my ways

--- AWS/iam-instance-roles/commvault-iam-service-role/main.tf
+++ AWS/iam-instance-roles/commvault-iam-service-role/main.tf
@@ -25,56 +25,20 @@ resource "aws_iam_policy" "wt-commvault-role-policy" {
   name   = "WT_Commvault_IAM_Role_Policy"
 }
 
-resource "aws_iam_policy_attachment" "wt-commvault-policy-attachement" {
-  name       = "WT_Commvault_Policy_Attachment"
+resource "aws_iam_role_policy_attachment" "wt-commvault-policy-attachement" {
   policy_arn = aws_iam_policy.wt-commvault-role-policy.arn
-  roles      = [aws_iam_role.wt-commvault-role.name, aws_iam_role.wt-commvault-glacier-role.name, aws_iam_role.wt-commvault-s3-role.name]
+  role       = aws_iam_role.wt-commvault-role.name
 }
 
-
-# Comvault Access to S3
-resource "aws_iam_role" "wt-commvault-s3-role" {
-  assume_role_policy = file("${path.module}/policies/ec2-trust.json")
-  description        = "IAM Service Role allowing access for Commvault to S3"
-  name               = "WT_Commvault_S3_Service_Role"
-  tags = {
-    Name        = "WT_Commvault_S3_Service_Role"
-    Owner       = var.owner
-    Managed     = var.managed
-    Environment = "All"
-    Internal    = ""
-    Cost        = var.cost
-    Division    = var.division
-    Department  = var.department
-  }
-}
 
 resource "aws_iam_policy" "wt-commvault-s3-role-policy" {
   policy = file("${path.module}/policies/commvault-s3-policy.json")
   name   = "WT_Commvault_S3_Role_Policy"
 }
 
-resource "aws_iam_policy_attachment" "wt-commvault-s3-policy-attachement" {
-  name       = "WT_Commvault_S3_Policy_Attachment"
+resource "aws_iam_role_policy_attachment" "wt-commvault-policy-attachement-2" {
   policy_arn = aws_iam_policy.wt-commvault-s3-role-policy.arn
-  roles      = [aws_iam_role.wt-commvault-s3-role.name]
-}
-
-# Comvault Access to Glacier
-resource "aws_iam_role" "wt-commvault-glacier-role" {
-  assume_role_policy = file("${path.module}/policies/ec2-trust.json")
-  description        = "IAM Service Role allowing access for Commvault to Glacier bucket"
-  name               = "WT_Commvault_Glacier_Service_Role"
-  tags = {
-    Name        = "WT_Commvault_Glacier_Service_Role"
-    Owner       = var.owner
-    Managed     = var.managed
-    Environment = "All"
-    Internal    = ""
-    Cost        = var.cost
-    Division    = var.division
-    Department  = var.department
-  }
+  role       = aws_iam_role.wt-commvault-role.name
 }
 
 resource "aws_iam_policy" "wt-commvault-glacier-role-policy" {
@@ -82,8 +46,7 @@ resource "aws_iam_policy" "wt-commvault-glacier-role-policy" {
   name   = "WT_Commvault_Glacier_Role_Policy"
 }
 
-resource "aws_iam_policy_attachment" "commvault-glacier-policy-attachement" {
-  name       = "WT_Commvault_Glacier_Policy_Attachment"
+resource "aws_iam_role_policy_attachment" "wt-commvault-policy-attachement-3" {
   policy_arn = aws_iam_policy.wt-commvault-glacier-role-policy.arn
-  roles      = [aws_iam_role.wt-commvault-glacier-role.name]
+  role       = aws_iam_role.wt-commvault-role.name
 }
