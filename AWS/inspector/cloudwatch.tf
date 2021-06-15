a7262b757cf9f178c7eb1a735720711b26b82418
wellingk <k.welling@wellcome.ac.uk>
2021-06-15 11:00:26 +0100

    added missing tags to be used in all accounts

--- AWS/inspector/cloudwatch.tf
+++ AWS/inspector/cloudwatch.tf
@@ -2,6 +2,20 @@ resource "aws_cloudwatch_event_rule" "aws-inspector" {
   name                = "aws-inspector-run-${var.inspector_name}"
   description         = "AWS Inspector run for ${var.inspector_name}"
   schedule_expression = var.inspector_schedule_expression
+  tags = {
+    Ansible       = var.Ansible
+    BackUps     = var.BackUps
+    Cost        = var.Cost
+    Department  = var.Department
+    Division    = var.Division
+    Environment    = var.Environment
+    Inspector = var.Inspector
+    Internal = var.Internal
+    Owner = var.Owner
+    PatchGroup = var.PatchGroup
+    Terraform = var.Terraform
+    Use = var.Use
+  }
 }
 
 resource "aws_cloudwatch_event_target" "aws-inspector" {
--- AWS/inspector/iam.tf
+++ AWS/inspector/iam.tf
@@ -21,5 +21,18 @@ resource "aws_iam_role_policy" "aws-inspector-policy" {
   name_prefix = "cloudwatch-event-inspector-${var.inspector_name}"
   role        = aws_iam_role.aws-inspector-role.id
   policy = file("${path.module}/policies/aws_inspector_role_policy.json")
-
+  tags = {
+    Ansible       = var.Ansible
+    BackUps     = var.BackUps
+    Cost        = var.Cost
+    Department  = var.Department
+    Division    = var.Division
+    Environment    = var.Environment
+    Inspector = var.Inspector
+    Internal = var.Internal
+    Owner = var.Owner
+    PatchGroup = var.PatchGroup
+    Terraform = var.Terraform
+    Use = var.Use
+  }
 }
