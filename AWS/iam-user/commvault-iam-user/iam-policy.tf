006d6a94fee10c903ce31da4d022b742fae007cc
wellingk <k.welling@wellcome.ac.uk>
2021-06-15 16:40:03 +0100

    updated a role used by Commvault

--- AWS/iam-user/commvault-iam-user/iam-policy.tf
+++ AWS/iam-user/commvault-iam-user/iam-policy.tf
@@ -3,4 +3,19 @@ resource "aws_iam_policy" "commvault-iam-user-policy" {
   policy      = file("${path.module}/policies/commvault-iam-user.json")
   description = "This policy allows access to the resources for Commvault"
   name        = "commvault-iam-user-policy"
+  tags = {
+    Name        = var.iam-user-name
+    Owner       = var.owner
+    Terraform     = var.terraform
+    Environment = "All"
+    Internal    = var.internal
+    Cost        = var.cost-a281
+    Division    = var.division
+    Department  = var.department
+    Inspector   = var.inspector
+    Use         = var.use-commvault
+    BackUps     = var.backups
+    Ansible     = var.ansible
+    PatchGroup  = var.patchgroup
+  }
 }
