64573e2a54694124c41e73994efd2b695dd4ef31
fvecellio-wellcome <f.vecellio@wellcome.ac.uk>
2023-07-05 09:48:21 +0100

    CHG0034394-ta-role-ssm-policy

--- AWS/iam-roles/ta-iam-role/main.tf
+++ AWS/iam-roles/ta-iam-role/main.tf
@@ -1,4 +1,4 @@
-# IAM Role for ThreatAware - CHG0034245
+# IAM Role for ThreatAware
 resource "aws_iam_role" "wt-ta-role" {
   assume_role_policy   = file("${path.module}/policies/wt-ta-role.json")
   description          = "ThreatAware API Connector to AWS"
@@ -27,4 +27,26 @@ resource "aws_iam_role_policy_attachment" "wt-ta-role-policy-attachment" {
 
   role       = aws_iam_role.wt-ta-role.name
   policy_arn = each.value
+}
+
+resource "aws_iam_policy" "wt-ta-policy" {
+  policy      = file("${path.module}/policies/wt-ta-policy.json")
+  description = "This policy allows SSM-Describe-Instance-Info for ThreatAware"
+  name        = "wt-ta-policy"
+
+  tags = {
+    Name        = "wt-ta-policy"
+    Cost        = var.Cost
+    Department  = var.Department
+    Division    = var.Division
+    Environment = var.Environment
+    Owner       = var.Owner
+    Terraform   = var.Terraform
+    Use         = var.Use
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "wt-ta-policy-attachment" {
+  policy_arn = aws_iam_policy.wt-ta-policy.arn
+  role       = aws_iam_role.wt-ta-role.name
 }
