46b5c83ecb0d04bc5b021d4c91086094720ef405
fvecellio-wellcome <f.vecellio@wellcome.ac.uk>
2024-01-11 13:58:23 +0000

    CHG0034933-panorama-instance-role

---
+++ AWS/iam-instance-roles/wt-panorama-instance-role/outputs.tf
@@ -0,0 +1,11 @@
+output "wt-panorama-instance-role-arn" {
+  value = aws_iam_role.wt-panorama-instance-role.arn
+}
+
+output "wt-panorama-instance-role-name" {
+  value = aws_iam_role.wt-panorama-instance-role.name
+}
+
+output "wt-panorama-instance-role-description" {
+  value = aws_iam_role.wt-panorama-instance-role.description
+}
---
+++ AWS/iam-instance-roles/wt-panorama-instance-role/variables.tf
@@ -0,0 +1,9 @@
+# Tagging variables
+variable "Cost" {}
+variable "Department" {}
+variable "Division" {}
+variable "Environment" {}
+variable "Owner" {}
+variable "Terraform" {}
+variable "Use" {}
+
