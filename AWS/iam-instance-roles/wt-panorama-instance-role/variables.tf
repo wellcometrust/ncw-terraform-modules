e7b6f25de6f1f60b19647066a1f6160d13d6a792
fvecellio-wellcome <f.vecellio@wellcome.ac.uk>
2024-01-17 13:49:09 +0000

    CHG0034960-panorama-role-ext

--- AWS/iam-instance-roles/wt-panorama-instance-role/variables.tf
+++ AWS/iam-instance-roles/wt-panorama-instance-role/variables.tf
@@ -3,7 +3,38 @@ variable "Cost" {}
 variable "Department" {}
 variable "Division" {}
 variable "Environment" {}
+variable "Monitoring" {}
 variable "Owner" {}
 variable "Terraform" {}
 variable "Use" {}
 
+
+variable "owner" {
+  description = "Owner of the resource"
+  default     = "Cloud Team"
+}
+
+variable "terraform" {
+  description = "How the resource is managed with Terraform"
+  default     = "True"
+}
+
+variable "division" {
+  description = "Operations Division"
+  default     = "Operations"
+}
+
+variable "department" {
+  description = "Department"
+  default     = "NCW"
+}
+
+variable "use-palo" {
+  description = "Use of resource"
+  default     = "Palo Alto Firewall"
+}
+
+variable "cost-a281" {
+  description = "NCW Cost Centre (Sam Horsman)"
+  default     = "A281"
+}
